bb2f68cb5338a6b0ae5b1fa296b42ab463ce1a5d
Valiant-Joshua Bolorunduro <valiantjoshuabolorunduro@gmail.com>
2023-05-04 21:25:29 +0100

    feat: Update App

--- pages/law/index.vue
+++ pages/law/index.vue
@@ -94,7 +94,7 @@ main {
 .mail--law {
     background-color: #1e1e1e;
     min-height: 100vh;
-    background-image: url("/law/background.svg");
+    background-image: url("/law/background.webp");
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
